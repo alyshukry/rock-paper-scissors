02de809ad91ae4c414a0c5930713511cfab04d78
Aly Shukry <alyshukry.dev+git@gmail.com>
2026-02-07 20:01:31 +0200

    chore: create server

---
+++ server.js
@@ -0,0 +1,10 @@
+import http from 'node:http'
+
+const PORT = 8000
+const server = http.createServer((req, res) => {
+    
+})
+
+server.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT + '...')
+})
