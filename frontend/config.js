f3ce2f8b785b3be73049601fb0b4ccf1f254fa19
Aly Shukry <alyshukry.dev@gmail.com>
2026-02-22 15:54:43 +0200

    bug: fix minor bug with local dev

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === 'localhost'
+const API_URL = window.location.hostname === 'localhost' || '127.0.0.1'
     ? 'http://localhost:8000'
     : 'https://rock-paper-scissors-kgfh.onrender.com'
 
